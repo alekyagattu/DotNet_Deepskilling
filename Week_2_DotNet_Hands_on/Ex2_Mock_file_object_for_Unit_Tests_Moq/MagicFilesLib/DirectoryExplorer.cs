778da3edcd66176f8e1e76a1cdbe11328152b59c
alekyagattu <gattualekya24@gmail.com>
2025-06-29 16:52:31 +0530

    Add files via upload

---
+++ Week_2_DotNet_Hands_on/Ex2_Mock_file_object_for_Unit_Tests_Moq/MagicFilesLib/DirectoryExplorer.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace MagicFilesLib
+{
+    public interface IDirectoryExplorer
+    {
+        ICollection<string> GetFiles(string path);
+    }
+
+    public class DirectoryExplorer : IDirectoryExplorer
+    {
+        public ICollection<string> GetFiles(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+    }
+}
