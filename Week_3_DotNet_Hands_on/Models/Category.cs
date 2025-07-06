f32b2debc8600d32c4867cb6ce0560881eceaf97
alekyagattu <gattualekya24@gmail.com>
2025-07-06 11:37:58 +0530

    Add files via upload

---
+++ Week_3_DotNet_Hands_on/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace Lab1.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public List<Product> Products { get; set; } = new();
+}
