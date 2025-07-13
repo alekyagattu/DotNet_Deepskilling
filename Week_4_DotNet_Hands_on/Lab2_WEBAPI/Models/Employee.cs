2964471513b09e2379625f0230a938bae1630f08
alekyagattu <gattualekya24@gmail.com>
2025-07-13 15:44:13 +0530

    Delete Week_4_DotNet_Hands_on/Lab2_WEBAPI/Models directory

--- Week_4_DotNet_Hands_on/Lab2_WEBAPI/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab2_WebApi.Models
-{
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public double Salary { get; set; }
-    }
-}
