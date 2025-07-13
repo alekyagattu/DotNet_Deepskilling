219361d0166e2ef46ed703083f93d2d36b917293
alekyagattu <gattualekya24@gmail.com>
2025-07-13 18:56:42 +0530

    Delete Week_4_DotNet_Hands_on/Lab1_WEBAPI/Models/Product.cs

--- Week_4_DotNet_Hands_on/Lab1_WEBAPI/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab1.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; } // nullable to avoid warning
-        public double Price { get; set; }
-    }
-}
