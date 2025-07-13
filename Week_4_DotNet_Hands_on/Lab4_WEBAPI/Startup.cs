47ad37b776f2d9f9c321c0311cd88bcb7608e323
alekyagattu <gattualekya24@gmail.com>
2025-07-13 17:55:16 +0530

    Add files via upload

---
+++ Week_4_DotNet_Hands_on/Lab4_WEBAPI/Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.OpenApi.Models;
+
+namespace Lab4
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Employee CRUD API",
+                    Version = "v1"
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment() || env.IsProduction() || env.IsStaging())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee CRUD API");
+                });
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
