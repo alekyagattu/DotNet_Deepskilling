6e95461d6286711d4cec657143b522774a47b757
alekyagattu <gattualekya24@gmail.com>
2025-07-13 15:37:57 +0530

    Add files via upload

---
+++ Week_4_DotNet_Hands_on/Lab2_WEBAPI/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace Lab2_WebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
