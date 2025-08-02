490991845af23dbb7ddf6120244b4b2b4c1a0554
alekyagattu <gattualekya24@gmail.com>
2025-08-02 11:28:13 +0530

    Add files via upload

---
+++ Week_7_DotNet_Hands_on/ReactJS_HOL_12/src/GuestPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GuestPage = () => (
+  <div style={{ padding: '20px' }}>
+    <h2>Welcome Guest!</h2>
+    <p>Here are some available flights:</p>
+    <ul>
+      <li>Flight A123 - New York to London</li>
+      <li>Flight B456 - Delhi to Dubai</li>
+      <li>Flight C789 - Tokyo to Paris</li>
+    </ul>
+    <p>Login to book your tickets.</p>
+  </div>
+);
+
+export default GuestPage;
